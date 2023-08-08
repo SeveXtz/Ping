3d7335c35cc3e76053fa88262052c28d3bab6f48
SeveXtz <119272391+SeveXtz@users.noreply.github.com>
2023-08-07 21:36:07 -0300

    Update Ping.py

--- Ping.py
+++ Ping.py
@@ -6,14 +6,14 @@ import os, colorama
 from colorama import *
 
 r = '\033[7;49;31m'
-rf = '\033[49;31m'
+rf = '\033[0;49;31m'
 g = '\033[49;92m'
 b = '\033[49;34m'
 w = '\033[0;49;37m'
 
 print (f'{r}Ocean Corporation >> ms.ping')
 p = "ping"
-s = input(f'{rf}[+]{w} Insira o alvo:\n>>{rf} ')
+s = input(f'{rf}[+]{w} Insira o alvo;\n>>{Fore.RED} ')
 print (f'{Fore.GREEN}')
 x = p + ' ' + s
 os.system(x)
